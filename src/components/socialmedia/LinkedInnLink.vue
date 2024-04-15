b78e14bbe9c27914255ea26ec7513359871f5500
Cedric <cedric.frachisse@gmail.com>
2024-04-15 16:00:32 +0200

    wip social

---
+++ src/components/socialmedia/LinkedInnLink.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <div class="github-profile-viewer">
+        <div class="user-profile">
+            <a href="" target="_blank">
+                <div>
+                    Here's my LinkedInn !
+                </div>
+            </a>
+        </div>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup></script>
+<script setup>
+import GitHubLink from '../components/socialmedia/GithubLink.vue'
+import LinkedInLink from '../components/socialmedia/LinkedInnLink.vue'
+</script>
 
 <template>
   <main>
-    <h1>Home</h1>
-    <p>Welcome to my PortFolio</p>
+    <h1>Welcome to my PortFolio</h1>
     <p>For now, most of these components are from tutorials. Special thank to HuXn Devweb.</p>
+    <div>
+      <p>Here's my GitHub:</p>
+      <GitHubLink />
+    </div>
+    <div>
+      <p>Here's my LinkedIn:</p>
+      <LinkedInLink />
+    </div>
   </main>
 </template>
